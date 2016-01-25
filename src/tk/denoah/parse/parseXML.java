a222ff59f2c096baf5ea04fc9ea0c3e49c5c9943
aivanov <na131@mail.ru>
2016-01-25 17:05:16 +0300

    Обвязка загрузки файлов

---
+++ src/tk/denoah/parse/parseXML.java
@@ -0,0 +1,15 @@
+package tk.denoah.parse;
+
+
+import org.w3c.dom.Node;
+
+import java.io.File;
+
+public class parseXML {
+    public Node parseXMLFile(File filename) {
+        Node returnNode = null;
+
+        return returnNode;
+    }
+
+}
--- src/tk/denoah/servlets/Upload.java
+++ src/tk/denoah/servlets/Upload.java
@@ -4,12 +4,19 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -18,7 +25,6 @@ import java.util.List;
 
 @WebServlet(name = "Upload")
 public class Upload extends HttpServlet {
-    private Random random = new Random();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //проверяем является ли запрос multipart/form-data
@@ -34,15 +40,17 @@ public class Upload extends HttpServlet {
         // Максимальный буфера данных в байтах,
         // при его привышении данные начнут записываться на диск во временную директорию
         // устанавливаем один мегабайт
-        factory.setSizeThreshold(1024*1024);
+        factory.setSizeThreshold(1024 * 1024);
 
         //Устанавливаем временную директорию
         File tempDir = (File) getServletContext().getAttribute("javax.servlet.context.tempdir");
         factory.setRepository(tempDir);
 
+        String dir = "C://output/";
+
         //Создаем загрузчик
         ServletFileUpload upload = new ServletFileUpload(factory);
-        upload.setSizeMax(1024*1024*10);
+        upload.setSizeMax(1024 * 1024 * 10);
 
         try {
             List items = upload.parseRequest(request);
@@ -52,24 +60,18 @@ public class Upload extends HttpServlet {
                 FileItem item = (FileItem) iter.next();
 
                 if (item.isFormField()) {
-                    response.getWriter().write("go!");
                     processFormField(item);
-                    response.getWriter().write("ok");
                 } else {
-                    response.getWriter().write("fileupload");
                     File uploadFile = null;
                     String path = item.getName();
                     response.getWriter().write(path);
-
-                    uploadFile = new File("C://output/", path);
-                    boolean created = uploadFile.createNewFile();
-                    item.write(uploadFile);
-
+                    uploadFile = new File(dir, path);
+                    uploadFile.createNewFile();
+                    String nodeString = parseMyXMLFile(uploadFile);
 
                     if (uploadFile.exists()) {
-                        response.getWriter().write("File Created");
+                        response.getWriter().write(nodeString);
                     }
-
 
 
                 }
@@ -85,19 +87,38 @@ public class Upload extends HttpServlet {
     }
 
     private void processFormField(FileItem item) {
-        System.out.println(item.getFieldName()+"="+item.getString());
+        System.out.println(item.getFieldName() + "=" + item.getString());
     }
 
-    private String processUploadFile(FileItem item) throws Exception{
-        File uploadFile = null;
-        String path = null;
-        do{
-            path = getServletContext().getRealPath("C://"+random.nextInt()+ item.getName());
-            uploadFile = new File(path);
-        } while (uploadFile.exists());
-        uploadFile.createNewFile();
-        item.write(uploadFile);
-        return path;
+    private String parseMyXMLFile(File file) throws ParserConfigurationException, IOException, SAXException {
+        Node resultNode = null;
+        String s = "";
+
+        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+        f.setValidating(false);
+        DocumentBuilder builder = f.newDocumentBuilder();
+        Document doc = builder.parse(file);
+        doc.getDocumentElement().normalize();
+
+        //Тут индусятина но времени нет, исправить как будет время
+        NodeList list = doc.getChildNodes();
+        Node childNode = list.item(1);
+        NodeList list1 = childNode.getChildNodes();
+        Node childNode1 = list1.item(0);
+        NodeList list2 = childNode1.getChildNodes();
+        Node childNode2 = list2.item(0);
+        NodeList list3 = childNode2.getChildNodes();
+        Node childNode3 = list3.item(1);
+        NodeList nodeList = childNode3.getChildNodes();
+        //конец индусятины
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node children = nodeList.item(i);
+            resultNode = children.getAttributes().item(0);
+            s += ";" + resultNode.getTextContent();
+        }
+
+        return s;
     }
 
 
